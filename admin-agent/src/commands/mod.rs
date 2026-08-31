50ec23a4122b0f745c049c24b2563745231cf920
Mateo lizcano <mateodevcode@gmail.com>
2026-08-31 11:03:31 +0200

    se comento docker

--- admin-agent/src/commands/mod.rs
+++ admin-agent/src/commands/mod.rs
@@ -71,7 +71,7 @@ pub fn execute_action(action: &str, payload: &Value) -> (bool, String) {
 pub const DYNAMIC_ACTIONS: &[&str] = &[
     "ram_info",
     "disk_space",
-    "docker_info",
+    // "docker_info",
     "uptime_check",
     "get_active_connections",
     // "network_threats",
